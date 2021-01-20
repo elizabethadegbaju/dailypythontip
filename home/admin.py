a3a83862b1d5028ae9d2ba7fb694aa34c9debb22
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-01-20 23:19:47 +0100

    feat: create Tip and TipLink models in home app

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
+"""
+Register dailypythontip home models with Django Admin
+"""
 from django.contrib import admin
 
-# Register your models here.
+from home.models import Tip, TipLink
+
+admin.site.register(Tip)
+admin.site.register(TipLink)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
+"""
+Models for dailypythontip home app
+"""
 from django.db import models
 
-# Create your models here.
+
+class Tip(models.Model):
+    """
+    This represents a single Daily Python Tip
+    """
+    timestamp = models.DateTimeField()
+    tip = models.TextField()
+    link = models.BooleanField(default=False)
+    author = models.CharField(max_length=255)
+    published = models.TextField()
+    total_likes = models.IntegerField()
+    total_retweets = models.IntegerField()
+
+
+class TipLink(models.Model):
+    """
+    This represents a single link in a Daily Python Tip
+    """
+    tip = models.ForeignKey(to=Tip, on_delete=models.CASCADE)
+    link = models.TextField()
