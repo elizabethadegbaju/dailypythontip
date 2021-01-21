75be1bf037df2499018c4d457b520327335af508
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-01-21 13:44:57 +0100

    feat: configure home app urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-"""dailypythontip URL Configuration
+"""dailypythontip home app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from home import views
+
+app_name = 'home'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('sync/', views.fetch_tweets, name='fetch-tweets'),
 ]
